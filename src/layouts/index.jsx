542f363fc02b4a3eb603eec4daa7ba2fdecfef6a
YVeselovskyi <19dynamokyiv94@gmail.com>
2025-02-18 17:49:49 +0200

    FIX: Layout upd

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,22 @@
 import { Header } from '@/components/Header';
 
 export const HomePageLayout = ({ children }) => (
-  <div className="relative">
-    <Header/>
-    {children}
-  </div>
+  <>
+    <div className="relative">
+      <div
+        className="absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat h-[768px]"
+        style={{ backgroundImage: 'url(/assets/hero-bg.webp)' }}
+      />
+      <Header/>
+      {children}
+    </div>
+    <div className="relative">
+      <div
+        className="absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat h-[670px]"
+        style={{ backgroundImage: 'url(/assets/footer-bg.png)' }}
+      />
+    </div>
+  </>
 );
 
 export const MainLayout = ({ children }) => (
