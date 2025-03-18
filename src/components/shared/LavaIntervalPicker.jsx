4a9fe2630a53e84dd9f2017d2c856f14b16c5e2f
YVeselovskyi <19dynamokyiv94@gmail.com>
2025-03-18 21:29:36 +0200

    FIXED: Utils

--- src/components/shared/LavaIntervalPicker.jsx
+++ src/components/shared/LavaIntervalPicker.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { ClockIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
-import { msToInterval, intervalToMs } from '@/lib/core.utils';
+import { msToInterval, intervalToMs } from '@/utils/core.utils';
 import { Button } from '@/components/ui/button';
 import {
   Popover,
