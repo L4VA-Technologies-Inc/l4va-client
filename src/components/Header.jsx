9f397e1119bdf841781e064d18bcaa54d4c889b0
YVeselovskyi <19dynamokyiv94@gmail.com>
2025-04-23 22:40:20 +0300

    WIP

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,6 +37,7 @@ export const Header = () => {
 
   const NavLink = ({ to, label }) => (
     <Link
+      activeProps={{ className: 'text-orange-500' }}
       className="min-w-[140px] text-center transition hover:text-orange-500"
       to={to}
       onClick={(e) => handleNavClick(to, e)}
