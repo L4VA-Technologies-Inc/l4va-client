92f6747e16649ae8afb52dc4ed2ca8acc35fd9d4
YVeselovskyi <19dynamokyiv94@gmail.com>
2025-08-26 12:08:58 +0300

    ADDED: Novu

--- src/routes/sandbox.jsx
+++ src/routes/sandbox.jsx
@@ -1,8 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Inbox } from '@novu/react';
+import { useNavigate } from 'react-router-dom';
 
-const SandboxComponent = () => {
-  return <div className="min-h-screen flex items-center justify-center">swap</div>;
-};
+function SandboxComponent() {
+  const navigate = useNavigate();
+
+  return (
+    <Inbox
+      applicationIdentifier="TeCf3Ck565vM"
+      subscriberId="68ad74bcd8e299ea20802270"
+      routerPush={path => navigate(path)}
+      appearance={{
+        variables: {
+          colorPrimary: '#DD2450',
+          colorForeground: '#0E121B',
+        },
+      }}
+    />
+  );
+}
 
 export const Route = createFileRoute('/sandbox')({
   component: SandboxComponent,
