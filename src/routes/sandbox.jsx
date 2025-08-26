0281d36188b5cc257ea341f4ec3f3a5e637fc6e3
YVeselovskyi <19dynamokyiv94@gmail.com>
2025-08-26 12:09:35 +0300

    REMOVED: router

--- src/routes/sandbox.jsx
+++ src/routes/sandbox.jsx
@@ -1,15 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Inbox } from '@novu/react';
-import { useNavigate } from 'react-router-dom';
 
 function SandboxComponent() {
-  const navigate = useNavigate();
-
   return (
     <Inbox
       applicationIdentifier="TeCf3Ck565vM"
       subscriberId="68ad74bcd8e299ea20802270"
-      routerPush={path => navigate(path)}
+      routerPush={path => console.log(path)}
       appearance={{
         variables: {
           colorPrimary: '#DD2450',
