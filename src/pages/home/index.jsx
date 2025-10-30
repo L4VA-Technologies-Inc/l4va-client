331622fe4b86d99e7c8a84f73a7f9a5a4c536e0b
Kyzfome <gumanist225@gmail.com>
2025-10-30 13:28:27 +0200

    FIXED: background width

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
   return (
     <>
       <div
-        className="absolute left-1/2 -translate-x-1/2 -top-16 z-[-1] w-full max-w-[1920px] min-h-[750px] bg-cover bg-bottom bg-no-repeat"
+        className="absolute left-1/2 -translate-x-1/2 -top-16 z-[-1] w-full min-h-[750px] bg-cover bg-bottom bg-no-repeat"
         style={{
           backgroundImage: 'url(/assets/vaults/banner-bg.webp)',
         }}
