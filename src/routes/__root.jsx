767d173c1644adc02c45e9b8704418b1e3a23cf0
Misha <gumanist225@gmail.com>
2026-03-11 14:26:56 +0200

    FIXED: frontend security

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,10 +1,18 @@
+import { lazy, Suspense } from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Toaster } from 'react-hot-toast';
 
 import MainLayout from '@/components/layout/MainLayout';
 import { NotFound } from '@/components/NotFound';
 import { Modal } from '@/lib/modals/modal.registry';
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : lazy(() =>
+      import('@tanstack/react-router-devtools').then(res => ({
+        default: res.TanStackRouterDevtools,
+      }))
+    );
 
 const RootComponent = () => {
   return (
@@ -12,7 +20,9 @@ const RootComponent = () => {
       <MainLayout>
         <Outlet />
       </MainLayout>
-      <TanStackRouterDevtools position="bottom-right" />
+      <Suspense>
+        <TanStackRouterDevtools position="bottom-right" />
+      </Suspense>
       <Modal />
       <Toaster
         toastOptions={{
